d2d2340d864b6a145b1b61d79bcc5baa0910ae57
Maksim Filippov <93541282+maxx4454@users.noreply.github.com>
2022-11-23 18:44:04 +0300

    Create uniswap.sol

---
+++ uniswap.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.4.11;
+
+contract IUniswapV3PoolEvents {
+    event Mint(
+        address sender,
+        address indexed owner,
+        int24 indexed tickLower,
+        int24 indexed tickUpper,
+        uint128 amount,
+        uint256 amount0,
+        uint256 amount1
+    );
+
+    event Burn(
+        address indexed owner,
+        int24 indexed tickLower,
+        int24 indexed tickUpper,
+        uint128 amount,
+        uint256 amount0,
+        uint256 amount1
+    );
+
+
+    event Swap(
+        address indexed sender,
+        address indexed recipient,
+        int256 amount0,
+        int256 amount1,
+        uint160 sqrtPriceX96,
+        uint128 liquidity,
+        int24 tick
+    );
+}
